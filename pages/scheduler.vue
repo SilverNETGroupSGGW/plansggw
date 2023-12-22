0bc356646d0b8fdc4a758c3b9c7d53276a6c554f
Grzegorz02@ <grzegorz.perun@gmail.com>
2023-12-22 20:17:31 +0100

    chore: formatting tweaks

--- pages/scheduler.vue
+++ pages/scheduler.vue
@@ -11,78 +11,81 @@ interface Lecture {
 
 // Misc
 function parseTime(time: string) {
-  const [hour, minute] = time.split(':').map(Number);
-  return (hour - 1) * 60 * 60 * 1000 + minute * 60 * 1000;
+  const [hour, minute] = time.split(':').map(Number)
+  return (hour - 1) * 60 * 60 * 1000 + minute * 60 * 1000
 }
 
 // Cells
 const timeRange = [...chunk(generateTimeInterval(new Date(2023, 0, 1, 8), new Date(2023, 0, 1, 20), 15), 2)]
   .map(([start, end]) => [
     start.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }),
-   end.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }),
+    end.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }),
   ])
   .map(([start, end]) => `${start} - ${end}`)
 
 function getCellId(group: string, time: string) {
-  const [start, end] = time.split(' - ');
-  return JSON.stringify({ group, start, end });
+  const [start, end] = time.split(' - ')
+  return JSON.stringify({ group, start, end })
 }
 
 // Mouse events
 const lectures = ref<Lecture[]>([])
 const isDragging = ref(false)
-let startMillis: number | null = null;
-let endMillis: number | null = null;
-let _group: string | null = null;
+let startMillis: number | null = null
+let endMillis: number | null = null
+let _group: string | null = null
 
 function onMouseDown(event: MouseEvent) {
-  if (event.button !== 0) return;
-  isDragging.value = true;
+  if (event.button !== 0)
+    return
+  isDragging.value = true
 }
 
 function onMouseUp(event: MouseEvent) {
-  if (event.button !== 0) return;
-  isDragging.value = false;
+  if (event.button !== 0)
+    return
+  isDragging.value = false
 
   if (startMillis !== null && endMillis !== null && _group !== null) {
     lectures.value.push({
       start: new Date(startMillis),
       end: new Date(endMillis),
       group: _group,
-    });
+    })
   }
 
-  startMillis = null;
-  endMillis = null;
-  _group = null;
+  startMillis = null
+  endMillis = null
+  _group = null
 }
 
-let lastCellId = '';
+let lastCellId = ''
 
 function onMouseMove(event: MouseEvent) {
-  if (!isDragging.value) return;
+  if (!isDragging.value)
+    return
 
-  const cell = event.target as HTMLTableCellElement;
-  if (!(cell instanceof HTMLTableCellElement)) return;
+  const cell = event.target as HTMLTableCellElement
+  if (!(cell instanceof HTMLTableCellElement))
+    return
 
-  const cellId = JSON.parse(cell.id);
+  const cellId = JSON.parse(cell.id)
 
-  if (cellId === lastCellId) return;
+  if (cellId === lastCellId)
+    return
 
-  lastCellId = cellId;
+  lastCellId = cellId
 
-  const currentStartMillis = parseTime(cellId.start);
-  const currentEndMillis = parseTime(cellId.end);
+  const currentStartMillis = parseTime(cellId.start)
+  const currentEndMillis = parseTime(cellId.end)
 
-  if (startMillis === null || currentStartMillis < startMillis) {
-    startMillis = currentStartMillis;
-  }
+  if (startMillis === null || currentStartMillis < startMillis)
+    startMillis = currentStartMillis
 
-  if (endMillis === null || currentEndMillis > endMillis) {
-    endMillis = currentEndMillis;
-  }
+  if (endMillis === null || currentEndMillis > endMillis)
+    endMillis = currentEndMillis
 
-  _group = cellId.group;
+  _group = cellId.group
 }
 </script>
 
@@ -122,11 +125,11 @@ function onMouseMove(event: MouseEvent) {
   </div>
 
   <div class="fixed bottom-0 right-0 z-10">
-      <div v-for="(lecture, index) in lectures" :key="index" class="border-gray-200 bg-white border text-gray-900 rounded-lg px-4 py-2 m-2">
-        <span class="font-semibold">{{ lecture.group }}, </span>
-        <span>
+    <div v-for="(lecture, index) in lectures" :key="index" class="m-2 rounded-lg border border-gray-200 bg-white px-4 py-2 text-gray-900">
+      <span class="font-semibold">{{ lecture.group }}, </span>
+      <span>
         {{ lecture.start.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }) }} - {{ lecture.end.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }) }}
-        </span>
-      </div>
+      </span>
+    </div>
   </div>
 </template>
