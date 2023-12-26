f7c129f5eab20b14ec2e6a8bbe391c804a86d44e
Grzegorz02@ <grzegorz.perun@gmail.com>
2023-12-26 21:11:31 +0100

    feat: custom dragging

--- pages/scheduler.vue
+++ pages/scheduler.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
-import type { InteractEvent, ResizeEvent } from '@interactjs/types'
-import interact from 'interactjs'
 import type { Lecture } from '~/types'
 
 // State
 const isDragging = ref(false)
 
 // Cells
+const groupCells = ref<HTMLDivElement[]>([])
 const groups = ref(['ISI-1', 'ISI-2', 'ISK', 'TM'])
-
-const headers = ref<HTMLTableCellElement[]>([])
-const headerHeight = ref(0) // constant
 
 function getBackgroundClass(lecture: Lecture) {
   switch (lecture.group[0]) {
@@ -37,129 +33,46 @@ const lectures = reactive<Lecture[]>([])
 const lectureCells = ref<HTMLDivElement[]>([])
 const { onPointerDown, onPointerMove, onPointerUp } = useMouse(lectures)
 
-onMounted(() => {
-  // Set header height
-  headerHeight.value = headers.value[0].offsetHeight
+// Drag
+const dragStart = ref({ x: 0, y: 0 })
 
-  // Apply interact.js
-  const _interact = interact('.lecture')
-    .draggable({
-      origin: 'parent',
-      modifiers: [
-        interact.modifiers.snap({
-          targets: [
-            interact.snappers.grid({ x: 48, y: headerHeight.value }),
-          ],
-          range: Number.POSITIVE_INFINITY,
-          relativePoints: [{ x: 0, y: 0 }],
-        }),
-      ],
-      inertia: false,
-    })
-    .on('dragstart', (event: InteractEvent) => {
-      const lecture = lectures.find(lecture => `lecture-${lecture.id.toString()}` === event.target.id)!
+function onDragDown(event: PointerEvent) {
+  isDragging.value = true
+  dragStart.value = { x: event.clientX, y: event.clientY }
 
-      if (!isDragging.value) {
-        isDragging.value = true
-        lectures.push({
-          ...lecture,
-          id: `ghost-${lecture.id}`,
-          group: [...lecture.group],
-          start: new Date(lecture.start),
-          end: new Date(lecture.end),
-          ghost: true,
-        })
-      }
-    })
-    .on('dragmove', (event: InteractEvent) => {
-      const lecture = lectures.find(lecture => `lecture-${lecture.id.toString()}` === event.target.id)!
+  // Add event listeners to window
+  window.addEventListener('pointermove', onDragMove)
+  window.addEventListener('pointerup', onDragUp)
+}
 
-      const dx = Math.ceil((lecture.left + event.dx) / 48) * 48
-      const dy = Math.ceil(lecture.top + event.dy)
+function onDragMove(event: PointerEvent) {
+  if (!isDragging.value)
+    return
 
-      if (dx < 0 || dy < 0)
-        return
+  // snap to 48px grid in X axis
+  const deltaX = Math.round((event.clientX - dragStart.value.x) / 48) * 48
 
-      lecture.left = dx
-      lecture.top = dy
+  // snap to groupCells height in Y axis
+  const deltaY = Math.round((event.clientY - dragStart.value.y) / groupCells.value[0].offsetHeight) * groupCells.value[0].offsetHeight
 
-      if (event.delta.y > 0) {
-        lecture.group.push(groups.value[groups.value.indexOf(lecture.group[lecture.group.length - 1]) + 1])
-        lecture.group.shift()
-      }
-      else if (event.delta.y < 0) {
-        lecture.group.unshift(groups.value[groups.value.indexOf(lecture.group[0]) - 1])
-        lecture.group.pop()
-      }
+  if (deltaX !== 0 || deltaY !== 0) {
+    for (const lecture of lectures) {
+      lecture.left += deltaX
+      lecture.top += deltaY
+    }
 
-      lecture.start = new Date(new Date(2023, 0, 1, 7, 45, 0, 0).getTime() + (lecture.left / 48 * 5 * 60 * 1000))
-      lecture.end = new Date(lecture.start.getTime() + (lecture.width / 48 * 5 * 60 * 1000))
-    })
-    .on('dragend', () => {
-      isDragging.value = false
-      lectures.splice(lectures.findIndex(lecture => lecture.ghost), 1)
-    })
-    .resizable({
-      origin: 'parent',
-      edges: { top: true, bottom: true, left: true, right: true },
-      modifiers: [
-        interact.modifiers.snap({
-          targets: [
-            interact.snappers.grid({ x: 48, y: headerHeight.value }),
-          ],
-          range: Number.POSITIVE_INFINITY,
-          relativePoints: [{ x: 0, y: 0 }],
-        }),
-      ],
-    })
-    .on('resizemove', (event: ResizeEvent) => {
-      const lecture = lectures.find(lecture => `lecture-${lecture.id.toString()}` === event.target.id)!
+    // Update dragStart based on the actual movement of the element
+    dragStart.value = { x: dragStart.value.x + deltaX, y: dragStart.value.y + deltaY }
+  }
+}
 
-      const dx = Math.ceil((lecture.left + event.deltaRect!.left) / 48) * 48
-      const dw = Math.round(Math.ceil((lecture.width + event.deltaRect!.width) / 48) * 48)
+function onDragUp() {
+  isDragging.value = false
 
-      if (dx < 0)
-        return
-
-      lecture.left = dx
-      lecture.width = dw
-
-      if (event.edges?.bottom) {
-        lecture.height += event.deltaRect!.bottom
-
-        if (event.deltaRect!.bottom > 0)
-          lecture.group.push(groups.value[groups.value.indexOf(lecture.group[lecture.group.length - 1]) + 1])
-
-        else if (event.deltaRect!.bottom < 0)
-          lecture.group.pop()
-      }
-      else if (event.edges?.top) {
-        const top = event.deltaRect!.top
-        if (top < 0)
-          return
-
-        lecture.height -= event.deltaRect!.top
-        lecture.top += top
-
-        if (event.deltaRect!.top < 0)
-          lecture.group.unshift(groups.value[groups.value.indexOf(lecture.group[0]) - 1])
-        else if (event.deltaRect!.top > 0)
-          lecture.group.shift()
-      }
-
-      lecture.group.sort()
-
-      lecture.start = new Date(new Date(2023, 0, 1, 7, 45, 0, 0).getTime() + (lecture.left / 48 * 5 * 60 * 1000))
-      lecture.end = new Date(lecture.start.getTime() + (lecture.width / 48 * 5 * 60 * 1000))
-    })
-
-  // Watch window size changes
-  window.addEventListener('resize', () => {
-    const _headerHeight = headers.value[0].offsetHeight
-    _interact.draggable({ modifiers: [interact.modifiers.snap({ targets: [interact.snappers.grid({ x: 48, y: _headerHeight })], range: Number.POSITIVE_INFINITY, relativePoints: [{ x: 0, y: 0 }] })] })
-    _interact.resizable({ modifiers: [interact.modifiers.snap({ targets: [interact.snappers.grid({ x: 48, y: _headerHeight })], range: Number.POSITIVE_INFINITY, relativePoints: [{ x: 0, y: 0 }] })] })
-  })
-})
+  // Remove event listeners from window
+  window.removeEventListener('pointermove', onDragMove)
+  window.removeEventListener('pointerup', onDragUp)
+}
 </script>
 
 <template>
@@ -192,13 +105,13 @@ onMounted(() => {
     <!-- here -->
     <div class="flex h-[calc(100vh-175px)]">
       <div class="flex h-full w-fit flex-col">
-        <div v-for="group in ['ISI-1', 'ISI-2', 'ISK', 'TM']" :id="group" :key="group" ref="headers" class="flex h-[calc(100%/4)] w-36 shrink-0 items-center justify-center border-r border-t border-gray-200 text-center text-xs text-gray-700">
+        <div v-for="group in ['ISI-1', 'ISI-2', 'ISK', 'TM']" :id="group" ref="groupCells" :key="group" class="flex h-[calc(100%/4)] w-36 shrink-0 items-center justify-center border-r border-t border-gray-200 text-center text-xs text-gray-700">
           {{ group }}
         </div>
       </div>
 
       <div class="relative flex flex-col">
-        <div v-for="lecture in lectures" :id="`lecture-${lecture.id?.toString()}`" ref="lectureCells" :key="lecture.id" :style="{ top: `${lecture.top}px`, left: `${lecture.left}px`, width: `${lecture.width}px`, height: `${lecture.height}px` }" class="lecture absolute pb-0.5 pr-0.5">
+        <div v-for="lecture in lectures" :id="`lecture-${lecture.id?.toString()}`" ref="lectureCells" :key="lecture.id" :style="{ top: `${lecture.top}px`, left: `${lecture.left}px`, width: `${lecture.width}px`, height: `${lecture.height}px` }" class="absolute pb-0.5 pr-0.5 hover:cursor-move" @pointerdown.prevent="onDragDown" @pointermove.prevent="onDragMove" @pointerup.prevent="onDragUp">
           <div class="flex h-full flex-col gap-2 rounded-md p-4" :class="[getBackgroundClass(lecture), { 'opacity-50': lecture.ghost, 'z-[1]': !lecture.ghost }]">
             <div class="flex flex-col gap-1">
               <span class="text-sm font-semibold text-white">{{ lecture.group.join(', ') }}</span>
