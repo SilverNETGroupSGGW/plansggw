cedfedbbbc0e5f3dbfa0ce49c1c35327ff371bba
Grzegorz02@ <grzegorz.perun@gmail.com>
2024-01-01 12:35:50 +0100

    feat(search): add no options slot

--- components/base/search.vue
+++ components/base/search.vue
@@ -37,7 +37,13 @@ const filteredOptions = computed(() => {
         <ComboboxInput class="flex h-12 flex-col items-start justify-center self-stretch rounded-lg border border-gray-300 py-4 pr-6 text-gray-700 transition-colors duration-200 ease-in-out placeholder:text-gray-400 hover:transition-all hover:duration-200 hover:ease-in-out focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 focus:transition-all focus:duration-200 focus:ease-in-out active:ring-2 active:ring-blue-600 disabled:bg-gray-100" v-bind="$attrs" :class="[icon ? 'pl-12' : 'pl-6']" @change="query = $event.target.value" />
 
         <TransitionRoot leave="transition ease-in duration-100" leave-from="opacity-100" leave-to="opacity-0" @after-leave="query = ''">
-          <ComboboxOptions class="absolute z-10 mt-4 max-h-60 w-full overflow-auto rounded-lg border border-gray-200 bg-white px-1 py-2 text-base shadow-lg focus:outline-none">
+          <div v-if="!filteredOptions.length" class="absolute z-10 mt-4 max-h-60 w-full overflow-auto rounded-lg border border-gray-200 bg-white px-1 py-2 text-base shadow-lg focus:outline-none">
+            <div class="relative cursor-default select-none rounded-lg px-5 py-2 text-gray-900">
+              <span class="block truncate">Nie znaleziono wyników</span>
+            </div>
+          </div>
+
+          <ComboboxOptions v-else class="absolute z-10 mt-4 max-h-60 w-full overflow-auto rounded-lg border border-gray-200 bg-white px-1 py-2 text-base shadow-lg focus:outline-none">
             <ComboboxOption v-for="(option, index) in filteredOptions" :key="index" v-slot="{ active }" as="template" :value="option.value">
               <li class="relative cursor-default select-none rounded-lg px-5 py-2" :class="{ 'bg-blue-600 text-white': active, 'text-gray-900': !active }">
                 <slot v-if="$slots.options" name="options" :option="option" :active="active" />
