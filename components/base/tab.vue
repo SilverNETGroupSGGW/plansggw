e52682c066d0aca8caf126dad96aee7ddc61d534
Grzegorz02@ <grzegorz.perun@gmail.com>
2024-01-09 13:19:35 +0100

    layout: update tabs hover

--- components/base/tab.vue
+++ components/base/tab.vue
@@ -10,7 +10,7 @@ const route = useRoute()
 </script>
 
 <template>
-  <NuxtLink :to="to" class="-mx-3 flex h-12 w-12 items-center justify-center gap-3 rounded-full font-medium transition-colors duration-200 ease-in-out group-hover:bg-indigo-500" :class="[route.path === to ? 'bg-indigo-400 text-indigo-100' : 'text-indigo-100']">
+  <NuxtLink :to="to" class="-mx-3 flex h-12 w-12 items-center justify-center gap-3 rounded-full font-medium transition-colors duration-200 ease-in-out group-hover:bg-indigo-500" :class="[route.path.startsWith(to) ? 'bg-indigo-400 text-indigo-100' : 'text-indigo-100']">
     <component :is="icon" class="h-5 w-5 stroke-2" />
   </NuxtLink>
 </template>
